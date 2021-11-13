190278c686ba9a03bb53a1f7e75432dfa3edb425
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-13 10:29:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0, x1=0, x2=1, xf=x1+x2;
+    cin >>n;
+    cout <<"1";
+    for (int i = 0; i <= n-1; ++i) {
+        cout<< xf << endl;
+        x1 = x2;
+        x2 = xf;
+        xf = x1 + x2;
+    }
+    return 0;
 }
